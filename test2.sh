8640c8f6aabd18b2efc3c8cfc1e053264283763c
mac-ci-jpg <75690645+mac-ci-jpg@users.noreply.github.com>
2020-12-08 15:14:45 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -vx
-echo testing6
-killall -9 tmate; tmate -S /tmp/tmate.sock new-session -d
-tmate -S /tmp/tmate.sock wait tmate-ready
-tmate -S /tmp/tmate.sock display
+echo testing7
